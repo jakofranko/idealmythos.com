c4cc1cd6b5f073688deb43f47fbb1ccdc175857e
Jake Franklin <jacob.d.franklin@gmail.com>
2022-05-15 14:08:05 -0700

    docs(about): add paragraph to about page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,10 +1,16 @@
 <div class="page">
     <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quaerat numquam esse cum quam quis deleniti voluptatibus sequi, laboriosam temporibus libero alias debitis, delectus a, eaque quod ratione id tenetur!</p>
+    <p>
+        Ideal Mythos is a creative studio dedicated to story telling. Our works
+        mostly consist of gaming materials and supplements for RPGs like
+        Dungeons & Dragons and Mutant Crawl Classics, but we have many other
+        projects on the horizon.
+    </p>
 </div>
 
 <style>
-    h1 {
+    h1,
+    h2 {
         font: var(--random-font);
     }
 </style>
--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -4,7 +4,7 @@
 
 <div class="page">
     <h1>Contact</h1>
-    <p>Feel free to reach out via social media, but <a href="mailto:info@idealmythos.com?subject=Reaching out to inquire about...">email is probably the most reliable way</a> to get in touch.</p>
+    <p>Feel free to reach out via social media, but <a href="mailto:info@idealmythos.com?subject=Reaching out to inquire about...">email is the most reliable way</a> to get in touch.</p>
     <Social />
 </div>
 
